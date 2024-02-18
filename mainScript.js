41fb9aadbc8e44b82017c9c19a15da7d8c3b1492
son_zephyrus_duo <son.engr.kr@gmail.com>
2024-02-18 23:36:14 +0900

    design update

--- mainScript.js
+++ mainScript.js
@@ -1,4 +1,7 @@
 function loadContent(tabName) {
+    
+
+
     fetch(`tabs/${tabName}.html`)
         .then(response => response.text())
         .then(html => {
@@ -8,9 +11,25 @@ function loadContent(tabName) {
             console.error('Error loading the content:', error);
             document.getElementById('content').innerHTML = '<p>Error loading content.</p>';
         });
+    
 }
 
 // 페이지 로드 시 'Home' 탭의 내용을 자동으로 로드
 document.addEventListener('DOMContentLoaded', function() {
+    const tabs = document.querySelectorAll('.tab-link');
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function() {
+            // 현재 active 클래스를 가진 모든 요소에서 active 클래스를 제거합니다.
+            tabs.forEach(t => t.classList.remove('active'));
+            
+            // 클릭된 탭에 active 클래스를 추가합니다.
+            this.classList.add('active');
+            
+            // 여기에 탭 내용을 로드하는 로직을 추가할 수 있습니다.
+            // 예: loadContent(this.getAttribute('data-tab'));
+        });
+    });
+    
     loadContent('home');
+    document.querySelector('.tab-link').classList.add('active'); // 첫 번째 탭을 활성화
 });
