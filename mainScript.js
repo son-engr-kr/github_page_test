ca079d7ab2b2a84afa02b1408ad0b96bedd970d7
son_zephyrus_duo <son.engr.kr@gmail.com>
2024-02-18 23:02:35 +0900

    add home tab

--- mainScript.js
+++ mainScript.js
@@ -1,19 +1,13 @@
-
-function openTab(evt, tabName) {
-    // 모든 탭 컨텐츠를 숨깁니다.
+function changeTab(evt, tabName) {
     var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
+    tabcontent = document.getElementsByClassName("tab-content");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
     }
-
-    // 모든 탭 링크를 비활성화합니다.
-    tablinks = document.getElementsByClassName("tablink");
+    tablinks = document.getElementsByClassName("tab-link");
     for (i = 0; i < tablinks.length; i++) {
         tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-
-    // 클릭된 탭과 연결된 탭 컨텐츠를 표시합니다.
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
 }
